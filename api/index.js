de6b29c4f86168cd0c68a09e907747e89fcd4234
Pratham Rawat <prathamrawat2004@gmail.com>
2024-10-11 21:26:41 +0530

    created database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// connecting to database
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to Database");
+})
+.catch((err)=>{
+    console.log("Not connected", err);
+});
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log("Server listening on port 3000");
 });
+
