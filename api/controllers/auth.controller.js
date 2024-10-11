e3d18457b0495bb477ce12e82c4ab6e6b5798c7d
Pratham Rawat <prathamrawat2004@gmail.com>
2024-10-11 23:55:05 +0530

    completed signup page

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password,10);
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+
+  try {
+    // saving the new user
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 
 const app = express();
@@ -16,9 +17,12 @@ mongoose
     console.log("Not connected", err);
   });
 
+// middleware
+app.use(express.json());
 
 // Routes
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
